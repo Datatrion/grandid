b7f6920deab084da0b35de2ab20faa0e5748fdd2
Robin Harms Oredsson <robin@betahaus.net>
2023-10-24 16:28:07 +0200

    Exceptions come in very different shapes, which doesn't match the docs.

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='grandid',
-    version='0.1.5',
-    description='GrandID Client',
-    author='Datatrion AB',
-    author_email='info@datatrion.se',
-    url='https://www.datatrion.se',
+    name="grandid",
+    version="0.1.5",
+    description="GrandID Client",
+    author="Datatrion AB",
+    author_email="info@datatrion.se",
+    url="https://www.datatrion.se",
     license="MIT",
     packages=find_packages(exclude=("tests",)),
     keywords=[],
     classifiers=[],
-    install_requires=[
-        'requests',
-        'six'
-    ],
+    install_requires=["requests", "six"],
     extras_require={
-        'testing': [
-            'requests_mock',
+        "testing": [
+            #'requests_mock',
         ],
     },
-    test_suite='grandid',
+    test_suite="grandid",
 )
